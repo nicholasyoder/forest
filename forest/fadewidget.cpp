42886a1c177c8a5fc1041e287f874f694e2de3ee
Nicholas Yoder <nicholasyoder@proton.me>
2025-03-15 19:01:14 -0400

    Fix fade in on start

--- forest/fadewidget.cpp
+++ forest/fadewidget.cpp
@@ -1,41 +1,23 @@
-/* BEGIN_COMMON_COPYRIGHT_HEADER
- * (c)LGPL3+
- *
- * Copyright: 2021 Nicholas Yoder
- *
- * This program or library is free software; you can redistribute it
- * and/or modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
-
- * You should have received a copy of the GNU Lesser General
- * Public License along with this library; if not, write to the
- * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301 USA
- *
- * END_COMMON_COPYRIGHT_HEADER */
-
 #include "fadewidget.h"
 #include <QScreen>
 
 fadewidget::fadewidget(QScreen *screen){
     setWindowOpacity(0.0);
     setWindowFlags(Qt::X11BypassWindowManagerHint);
+    setGeometry(screen->geometry());
     wscreen = screen;
+    getimage();
 }
 
 void fadewidget::paintEvent(QPaintEvent *){
-    QRectF target1(0.0, 0.0, this->width(), this->height());
+    int w = width(); int h = height();
+
+    QRectF target1(0.0, 0.0, w, h);
     QRectF source1(0.0, 0.0, backimage->width(), backimage->height());
     QPainter painter1(this);
     painter1.drawImage(target1, *backimage, source1);
 
-    QRectF target(0.0, 0.0, this->width(), this->height());
+    QRectF target(0.0, 0.0, w, h);
     QRectF source(0.0, 0.0, wallpaper->width(), wallpaper->height());
     QPainter painter(this);
     painter.setOpacity(opacity);
@@ -47,21 +29,9 @@ void fadewidget::getimage(){
     *backimage = wscreen->grabWindow(QApplication::desktop()->winId(), wscreen->geometry().x(), wscreen->geometry().y(), width(), height()).toImage();
 
     QSettings settings("Forest","Forest");
-    QString imode = settings.value("desktop/imagemode", "Fill").toString();
-    if (imode == "Fill")
-        imagemode = Fill;
-    else if (imode == "Fit")
-        imagemode = Fit;
-    else if (imode == "Stretch")
-        imagemode = Stretch;
-    else if (imode == "Tile")
-        imagemode = Tile;
-    else if (imode == "Center")
-        imagemode = Center;
-
+    imagemode = WALLPAPER_MODE(settings.value("desktop/imagemode", Fill).toInt());
     QString wallpaper_file = settings.value("desktop/wallpaper", "").toString();
-    if(!wallpaper_file.isEmpty())
-        wallpaper = miscutills::get_wallpaper_scaled(wallpaper_file, imagemode, this->size());
+    wallpaper = miscutills::get_wallpaper_scaled(wallpaper_file, imagemode, this->size());
 
     setWindowOpacity(1.0);
 }
@@ -71,23 +41,9 @@ void fadewidget::start(){
     t->start(40);
 }
 
-void fadewidget::blackout(){
-    delete t;
-    t = new QTimer;
-    connect(t, SIGNAL(timeout()), this, SLOT(fadeblack()));
-    t->start(40);
-}
-
-void fadewidget::stop(){
-    delete t;
-    t = new QTimer;
-    connect(t, SIGNAL(timeout()), this, SLOT(fadeout()));
-    t->start(40);
-}
-
 void fadewidget::fadein(){
     if (opacity < 1.0){
-        opacity = opacity + 0.05;
+        opacity += 0.05;
         update();
     }
     else{
@@ -95,26 +51,5 @@ void fadewidget::fadein(){
         delete backimage;
         delete wallpaper;
         delete this;
-    }
-}
-
-void fadewidget::fadeblack(){
-    if (opacity < 1.0){
-        opacity = opacity + 0.05;
-        update();
-    }
-    else{
-        t->stop();
-    }
-}
-
-void fadewidget::fadeout(){
-    if (opacity > 0.0){
-        opacity = opacity - 0.05;
-        update();
-    }
-    else{
-        t->stop();
-        this->close();
     }
 }
--- forest/fadewidget.h
+++ forest/fadewidget.h
@@ -1,25 +1,3 @@
-/* BEGIN_COMMON_COPYRIGHT_HEADER
- * (c)LGPL3+
- *
- * Copyright: 2021 Nicholas Yoder
- *
- * This program or library is free software; you can redistribute it
- * and/or modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
-
- * You should have received a copy of the GNU Lesser General
- * Public License along with this library; if not, write to the
- * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301 USA
- *
- * END_COMMON_COPYRIGHT_HEADER */
-
 #ifndef FADEWIDGET_H
 #define FADEWIDGET_H
 
@@ -32,9 +10,7 @@
 
 #include "miscutills.h"
 
-
-class fadewidget : public QWidget
-{
+class fadewidget : public QWidget{
     Q_OBJECT
 
 public:
@@ -48,16 +24,12 @@ public:
 public slots:
     void getimage();
     void start();
-    void blackout();
-    void stop();
 
 protected:
     void paintEvent(QPaintEvent *);
 
 private slots:
     void fadein();
-    void fadeblack();
-    void fadeout();
 
 private:
     WALLPAPER_MODE imagemode;
