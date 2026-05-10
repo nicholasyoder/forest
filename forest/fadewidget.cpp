83517ad037b0311e1af059bb909805980c07668a
Nicholas Yoder <nicholasyoder@proton.me>
2026-05-10 11:58:13 -0400

    Fix fadein screenshots

--- forest/fadewidget.cpp
+++ forest/fadewidget.cpp
@@ -28,7 +28,8 @@ void fadewidget::paintEvent(QPaintEvent *){
 
 void fadewidget::getimage(){
     //grab screenshot - maybe this can get deleted after i write a session manager
-    *backimage = wscreen->grabWindow(0, wscreen->geometry().x(), wscreen->geometry().y(), width(), height()).toImage();
+    QRect screenRect = wscreen->geometry();
+    *backimage = qApp->primaryScreen()->grabWindow(0, screenRect.x(), screenRect.y(), screenRect.width(), screenRect.height()).toImage();
 
     QSettings settings("Forest","Forest");
     imagemode = WALLPAPER_MODE(settings.value("desktop/imagemode", Fill).toInt());
